136f40dc9931e262d59baa09dceda0e7a3129def
flowerwrong <sysuyangkang@gmail.com>
2017-12-09 17:18:57 +0800

    fix rawfile unsafe on android

--- tcpip/link/rawfile/rawfile_unsafe_amd64.go
+++ tcpip/link/rawfile/rawfile_unsafe_amd64.go
@@ -1,3 +1,4 @@
+// +build linux
 // Copyright 2016 The Netstack Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
